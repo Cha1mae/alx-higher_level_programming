c34a7c032535b7d1bdee2a6046b50f1b1f307763
Cha1mae <chaimastaifa@gmail.com>
2023-10-03 09:07:52 -0700

    print tebahpla

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-
-for char in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(char) if (ord('z') - char) % 2 == 0 else chr(char - 32)), end="")
-
-print()
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
