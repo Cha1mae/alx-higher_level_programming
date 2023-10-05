d021545911df9c7ad14707e2e164ff6990d9fee0
Cha1mae <chaimastaifa@gmail.com>
2023-10-05 11:40:16 -0700

    hidden discovery

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """prints the result of add in args"""
 if __name__ == "__main__":
-
     import sys
+
     total = 0
     for arg in sys.argv[1:]:
-       total += int(arg)
-    print("{}".format(total))
+        total += int(arg)
+
+        print("{}".format(total))
