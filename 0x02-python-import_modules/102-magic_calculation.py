80418deb0e8d6dd144e27f109cef6fc2c067a17d
Cha1mae <chaimastaifa@gmail.com>
2023-10-05 16:32:09 -0700

    8. ByteCode -> Python #3 task

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+
+
 def magic_calculation(a, b):
-    """magic calc"""
+    """Match bytecode provided by Holberton School."""
     from magic_calculation_102 import add, sub
 
     if a < b:
