6019250cc7e6da074ebf9d5eb2f3eb7bb612961d
Cha1mae <chaimastaifa@gmail.com>
2023-10-07 10:19:09 -0700

    no c

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def no_c(my_string):
+    no_c_string = ""
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            no_c_string += char
+    return (no_c_string)
