1c0797943c5ae1a5f9e61723844d025826ed4836
Cha1mae <chaimastaifa@gmail.com>
2023-10-07 08:50:58 -0700

    print list int

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-"""prints every int in a line"""
-
 def print_list_integer(my_list=[]):
-    for n in range(len(my_list)):
-        print("{:d}".format(my_list[n]))
+    for n in my_list:
+        print("{:d}".format(n))
