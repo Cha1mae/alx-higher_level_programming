ccc5fa2fd285e48f35a10946846c2b64cd36fae3
Cha1mae <chaimastaifa@gmail.com>
2023-10-08 09:16:55 -0700

    div by 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if not my_list:
+        return None
+    
+    list2divs = []
+    for i in my_list:
+        if (i % 2) == 0:
+            list2divs.append(True)
+        else:
+            list2divs.append(False)
+    
+    return list2divs
