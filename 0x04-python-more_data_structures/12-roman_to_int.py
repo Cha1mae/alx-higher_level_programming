8cd35d0fb5572bffaf2dd77890770d1d918271be
Cha1mae <chaimastaifa@gmail.com>
2023-10-11 11:52:20 -0700

    roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,11 +2,9 @@
 def subtract_smaller(list_num):
     total_to_subtract = 0
     max_in_list = max(list_num)
-
     for num in list_num:
         if max_in_list > num:
             total_to_subtract += num
-
     return (max_in_list - total_to_subtract)
 
 
@@ -15,11 +13,9 @@ def roman_to_int(roman_string):
         return (0)
     if not isinstance(roman_string, str):
         return (0)
-
     roman_numerals = {'I': 1, 'V': 5, 'X': 10,
         'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     numeral_keys = list(roman_numerals.keys())
-
     result = 0
     pre_value = 0
     cur_values = [0]
@@ -33,6 +29,5 @@ def roman_to_int(roman_string):
                     cur_values.append(roman_numerals.get(char))
 
                 pre_value = roman_numerals.get(char)
-
     result += subtract_smaller(cur_values)
     return (result)
