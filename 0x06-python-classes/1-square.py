1e0052e7c7799a6bbadd109d6708d3f298705401
Cha1mae <chaimastaifa@gmail.com>
2023-10-24 09:51:54 -0700

    trying

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a square and Initialises the data """
+
+
+class Square:
+    """square def"""
+
+    def __init__(self, size):
+	"""the constr
+
+	Args:
+	    size: the leghnt of side of square
+	"""
+        self.__size = size
