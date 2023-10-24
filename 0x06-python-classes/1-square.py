019359ff8238d04a973033485be7655388907541
Cha1mae <chaimastaifa@gmail.com>
2023-10-24 09:55:55 -0700

    square square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Defines a square and Initialises the data """
+"""Defines a square and initializes the data"""
 
 
 class Square:
-    """square def"""
+    """Square class definition"""
 
     def __init__(self, size):
-	"""the constr
+        """Constructor for Square class
 
-	Args:
-	    size: the leghnt of side of square
-	"""
+        Args:
+            size (int): The length of the side of the square
+        """
         self.__size = size
