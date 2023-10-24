004915951952f5e394842a9df74b4e84587828f1
Cha1mae <chaimastaifa@gmail.com>
2023-10-24 11:14:50 -0700

    trying if this work

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""square mod"""
+
+
+class Square:
+    def __init__(self, size=0):
+        """
+        Initialize the Square instance with a given size.
+
+        Parameters
+        ----------
+        size : int, optional
+            The size of the square (default is 0).
+
+        Raises
+        ------
+        TypeError
+            If size is not an integer.
+        ValueError
+            If size is less than 0.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        Getter method to retrieve the size of the square.
+
+        Returns
+        -------
+        int
+            The size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter method to set the size of the square.
+
+        Parameters
+        ----------
+        value : int
+            The size of the square.
+
+        Raises
+        ------
+        TypeError
+            If value is not an integer.
+        ValueError
+            If value is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+        Calculate the area of the square.
+
+        Returns
+        -------
+        int
+            The area of the square.
+        """
+        return self.__size ** 2
