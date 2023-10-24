fa6870c4c87307bc85acd612ece243d582b1630c
Cha1mae <chaimastaifa@gmail.com>
2023-10-24 10:19:17 -0700

    square square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,6 +8,8 @@ class Square:
 
     def __init__(self, size=0):
         """Initializes the data"""
+
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
