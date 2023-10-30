c746a79e6256556235ca6d87a5b2ac81a23ab9d2
Cha1mae <chaimastaifa@gmail.com>
2023-10-30 14:00:45 -0700

    trying

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -6,21 +6,29 @@ Define a class Rectangle that defines a rectangle
 
 
 class Rectangle:
-    """Rectangle class"""
+    """
+    Rectangle class
+    """
 
     def __init__(self, width=0, height=0):
-        """Initialize Rectangle"""
+        """
+        Initialize Rectangle
+        """
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """Retrieve the width of Rectangle"""
+        """
+        Retrieve the width of Rectangle
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Set the width of Rectangle"""
+        """
+        Set the width of Rectangle
+        """
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -29,12 +37,16 @@ class Rectangle:
 
     @property
     def height(self):
-        """Retrieve the height of Rectangle"""
+        """
+        Retrieve the height of Rectangle
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Set the height of Rectangle"""
+        """
+        Set the height of Rectangle
+        """
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
@@ -42,17 +54,23 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """Calculate the area of Rectangle"""
+        """
+        Calculate the area of Rectangle
+        """
         return self.__width * self.__height
 
     def perimeter(self):
-        """Calculate the perimeter of Rectangle"""
+        """
+        Calculate the perimeter of Rectangle
+        """
         if self.__width == 0 or self.__height == 0:
             return 0
         return (self.__width * 2) + (self.__height * 2)
 
     def __str__(self):
-        """Print the rectangle with the character '#'"""
+        """
+        Print the rectangle with the character '#'
+        """
         rect_str = ""
         if self.__width != 0 and self.__height != 0:
             rect_str += '\n'.join('#' * self.__width
