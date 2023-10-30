569d6be1c43cac6640652cc477f8842553d1c346
Cha1mae <chaimastaifa@gmail.com>
2023-10-30 13:53:35 -0700

    tring 3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -20,7 +20,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """sett the width of Rectangle"""
-        if not isinstance(value, int):
+        if type(Value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -34,7 +34,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """sett the height of Rectangle"""
-        if not isinstance(value, int):
+        if type(Value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
@@ -48,17 +48,12 @@ class Rectangle:
         """Cal the perimeter of Rectangle"""
         if self.__width == 0 or self.__height == 0:
             return 0
-        return 2 * (self.__width + self.__height)
+        return (self.__width * 2) + (self.__width * 2)
 
     def __str__(self):
         """let’s print the rectangle with the character '#'"""
         rect_str = ""
-        if self.__width == 0 or self.__height == 0:
-            return rect_str
-        for i in range(self.__height):
-            rect_str += '#' * self.__width + '\n'
-        return rect_str[:-1]
-
-    def __repr__(self):
-        """let’s return the object representation of Rectangle"""
-        return "Rectangle({}, {})".format(self.__width, self.__height)
+        if self.__width != 0 and self.__height != 0:
+            rect_str += '\n'.join('#' * self.__width
+			        for j in range(self.__height))
+        return string
