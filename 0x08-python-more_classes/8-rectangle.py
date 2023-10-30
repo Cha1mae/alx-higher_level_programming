b943179105a479dcbad2a38680c2b85d502293e8
Cha1mae <chaimastaifa@gmail.com>
2023-10-30 13:29:46 -0700

    please work

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -6,19 +6,19 @@ Define a class Rectangle that defines a rectangle
 
 
 class Rectangle:
-    """Rectangle class ugh"""
+    """Rectangle class plz work"""
     number_of_instances = 0
     print_symbol = '#'
 
     def __init__(self, width=0, height=0):
-        """let’s int Rectangle"""
+        """Init Rectangle"""
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
 
     @property
     def width(self):
-        """we b fetshing the width of Rectangle"""
+        """fetsh the width of Rectangle"""
         return self.__width
 
     @width.setter
@@ -32,7 +32,7 @@ class Rectangle:
 
     @property
     def height(self):
-        """fetshin the height of Rectangle"""
+        """fetsh the height of Rectangle"""
         return self.__height
 
     @height.setter
@@ -45,17 +45,17 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """calculating the area of Rectangle"""
+        """calc the area of Rectangle"""
         return self.__width * self.__height
 
     def perimeter(self):
-        """calculating the perimeter of Rectangle"""
+        """calc the perimeter of Rectangle"""
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
-        """Pwin the rectangle with the char(s) stored in print_symbol"""
+        """Pwint the rectangle with the char(s) stored in print_symbol"""
         rect_str = ""
         if self.__width == 0 or self.__height == 0:
             return rect_str
@@ -64,7 +64,7 @@ class Rectangle:
         return rect_str[:-1]
 
     def __repr__(self):
-        """returns a string rep of the rectangle for recreation"""
+        """Ret a string representation of the rectangle for recreation"""
         return "Rectangle({}, {})".format(self.__width, self.__height)
 
     def __del__(self):
@@ -74,7 +74,7 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        """returns the biggest rectangle based on the area"""
+        """Returns the biggest rectangle based on the area"""
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
@@ -82,10 +82,11 @@ class Rectangle:
         return rect_1 if rect_1.area() >= rect_2.area() else rect_2
 
 
-my_rectangle_1 = Rectangle(8, 4)
-my_rectangle_2 = Rectangle(2, 1)
+if __name__ == "__main__":
+    my_rectangle_1 = Rectangle(8, 4)
+    my_rectangle_2 = Rectangle(2, 1)
 
-if my_rectangle_1 is Rectangle.bigger_or_equal(my_rectangle_1, my_rectangle_2):
-    print("my_rectangle_1 is bigger than or equal to my_rectangle_2")
-else:
-    print("my_rectangle_2 is bigger than my_rectangle_1")
+    if my_rectangle_1 is Rectangle.bigger_or_equal(my_rectangle_1, my_rectangle_2):
+        print("my_rectangle_1 is bigger or equal to my_rectangle_2")
+    else:
+        print("my_rectangle_2 is bigger than my_rectangle_1")
