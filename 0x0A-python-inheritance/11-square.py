cb41b26044f28b457937b1a6d54bd961e12d2dd9
Cha1mae <chaimastaifa@gmail.com>
2023-11-06 09:20:29 -0800

    square pie

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class Square that inherits from Rectangle"""
+
+    def __init__(self, size):
+        """Starts private size attribute"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        """Return the square description"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
