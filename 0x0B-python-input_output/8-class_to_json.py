1330d1f9578a1d743f33847b430b2382b306b696
Cha1mae <chaimastaifa@gmail.com>
2023-11-07 09:12:06 -0800

    class to json

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+module task 8
+"""
+
+
+def class_to_json(obj):
+    """
+    Returns the dictionary description with simple data structure
+    """
+    return obj.__dict__
+
+
+if __name__ == "__main__":
+    pass
