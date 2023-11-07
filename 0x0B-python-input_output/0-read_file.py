e8f71eaba378730f6b4e3aa26f8a82c97e8c9cf9
Cha1mae <chaimastaifa@gmail.com>
2023-11-07 08:28:57 -0800

    read a pie file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+takes (UTF8) text file and prints it to stdout
+"""
+
+
+def read_file(filename=""):
+    """
+    Reads a text file (UTF8) and prints it to stdout
+    """
+    with open(filename, encoding='utf-8') as file:
+        print(file.read(), end='')
+
+
+if __name__ == "__main__":
+    read_file("my_file_0.txt")
