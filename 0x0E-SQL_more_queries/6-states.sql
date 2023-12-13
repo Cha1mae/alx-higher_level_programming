625f91d01ae88c727c36ad6501da2f6fd669457f
Cha1mae <chaimastaifa@gmail.com>
2023-12-13 08:06:13 -0800

    state

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,12 +1,8 @@
 -- 6-states.sql
 -- with the state table this create the table
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-
-CREATE TABLE IF NOT EXISTS states (
-    id INT AUTO_INCREMENT,
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
+    PRIMARY KEY(`id`),
+    `id`   INT          NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(256) NOT NULL
 );
-
-INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas");
