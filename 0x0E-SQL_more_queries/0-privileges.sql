06ae1df1e7a5f74c727d10c9ca7f059c01d8152b
Cha1mae <chaimastaifa@gmail.com>
2023-12-13 07:41:30 -0800

    trying

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,6 +1,5 @@
 -- 0-privileges.sql
 -- list of priviledge of the users
-SELECT CONCAT('\'',user,'\'@\'',host,'\'') AS User, 
-       Select_priv, Insert_priv, Update_priv, Delete_priv, Create_priv, Drop_priv 
+SELECT CONCAT('SHOW GRANTS FOR ''', user, '''@''', host, ''';') 
 FROM mysql.user 
 WHERE user IN ('user_0d_1', 'user_0d_2');
