c860f263f2aeaffe8601854562e399e39820f1ef
Cha1mae <chaimastaifa@gmail.com>
2023-12-13 10:38:51 -0800

    fixing errors

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,7 @@
 -- 9-cities_by_state_join.sql
 --sort in order of < cities.id.
-SELECT c.`id`, c.`name`, s.`name`
-  FROM `cities` AS c
-       INNER JOIN `states` AS s
-       ON c.`state_id` = s.`id`
- ORDER BY c.`id`;
+SELECT c.id, c.name, s.name
+FROM cities AS c
+INNER JOIN states AS s
+ON c.state_id = s.id
+ORDER BY c.id ASC;
