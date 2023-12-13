eeb84eb2fa22ab3a403cfd8fdec275f0e48f3226
Cha1mae <chaimastaifa@gmail.com>
2023-12-13 08:14:31 -0800

    citiz by state

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- 9-cities_by_state_join.sql
+--sort in order of ascending cities.id.
+SELECT c.`id`, c.`name`, s.`name`
+  FROM `cities` AS c
+       INNER JOIN `states` AS s
+       ON c.`state_id` = s.`id`
+ ORDER BY c.`id`;
