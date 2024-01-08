68f16029adfb29e85f504a760910cc5e26fbd0cd
Cha1mae <chaimastaifa@gmail.com>
2024-01-08 08:40:47 -0800

    loop lang

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// tli lines by usin aray stwing or a loop
+const args = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const arg in args) {
+  console.log(args[arg]);
+}
