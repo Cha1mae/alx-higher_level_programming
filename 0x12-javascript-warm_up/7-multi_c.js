321c9f6cf8efa68912929667aca977a5f57c9781
Cha1mae <chaimastaifa@gmail.com>
2024-01-08 08:46:20 -0800

    x num

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// x time c is fun
+const x = Math.floor(Number(process.argv[2]));
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let j = 0; j < x; j++) {
+    console.log('C is fun');
+  }
+}
