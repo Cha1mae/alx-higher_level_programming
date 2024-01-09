c9af58a2069b8b7c17b3c120782668fdffdc664c
Cha1mae <chaimastaifa@gmail.com>
2024-01-09 04:35:53 -0800

    drwaing a rec

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// defining a rec again and print it usin x
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+  }
+
+  print () {
+    for (let a = 0; a < this.height; a++) console.log('X'.repeat(this.width));
+  }
+};
