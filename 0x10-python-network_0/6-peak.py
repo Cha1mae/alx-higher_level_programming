6c3d6a38de9942b928c8e0c7e4075daa3ed5d3b5
Cha1mae <chaimastaifa@gmail.com>
2024-01-25 07:52:02 -0800

    t3sbt

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
 """ Module to find a peak num in a list of unsorted ints"""
 
-
 def find_peak(list_of_integers):
     """the function will do the job for us"""
     if not list_of_integers:
         return None
 
-    length = len(list_of_integers)
-    if length == 1:
-        return list_of_integers[0]
-    elif length == 2:
-        return max(list_of_integers)
+    low, high = 0, len(list_of_integers) - 1
 
-    m = length // 2
-    p = list_of_integers[m]
-    if p > list_of_integers[m - 1] and p > list_of_integers[m + 1]:
-        return p
-    elif p < list_of_integers[m - 1]:
-        return find_peak(list_of_integers[:m])
-    else:
-        return find_peak(list_of_integers[m + 1:])
+    while low < high:
+        mid = (low + high) // 2
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            high = mid
+        else:
+            low = mid + 1
+
+    return list_of_integers[low]
