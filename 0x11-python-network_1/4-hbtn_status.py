d6f6176e8672171e1152ebe9bb90c94d9ef9c310
Cha1mae <chaimastaifa@gmail.com>
2024-01-26 09:50:21 -0800

    i simplified my code

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,24 +1,15 @@
 #!/usr/bin/python3
+"""i siplified my previous code in hope of
+having green checks"""
 import requests
 
+"""The URL provided"""
+url = "https://alx-intranet.hbtn.io/status"
 
-def fetch_lurl():
-    """
-    Fetches the status from the link  using request
-    Displays the body response with the info
-    """
-    """The URL provided"""
-    url = "https://alx-intranet.hbtn.io/status"
+"""GET the URL"""
+rec = requests.get(url)
 
-    """GET the URL"""
-    rec = requests.get(url)
-
-    """Display the body info"""
-    print("Body response:")
-    print("\t- type:", type(rec.text))
-    print("\t- content:", rec.text)
-
-
-if __name__ == "__main__":
-    """Call the fetch_lurl function"""
-    fetch_lurl()
+"""Display the body info"""
+print("Body response:")
+print("\t- type:", type(rec.text))
+print("\t- content:", rec.text)
